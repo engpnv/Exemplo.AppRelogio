8a2ba270eeb0090e20db5a63abb2ec666988c35a
eng.pnv <pablo.vargas@unir.br>
2021-10-19 09:13:33 -0400

    App Relógio v1.1

--- app/src/main/java/com/example/exemploapprelogio/FullscreenActivity2.java
+++ app/src/main/java/com/example/exemploapprelogio/FullscreenActivity2.java
@@ -1,14 +1,22 @@
 package com.example.exemploapprelogio;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Calendar;
 
@@ -19,38 +27,102 @@ public class FullscreenActivity2 extends AppCompatActivity {
 
     private Handler handler = new Handler();
     private Runnable runnable;
-//
+    //
     //parte 5
-    private boolean runnableStopped =false;
+    private boolean runnableStopped = false;
+    private boolean bateriaChecked = true;
+    //
+//parte v1.1
+    private BroadcastReceiver bateriaReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int nivel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            viewHolder.tv_nivelBateria.setText(String.valueOf(nivel) + "%");
+
+        }
+    };
 //
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen2);
         viewHolder.tv_horasMinutos = findViewById(R.id.tv_horasMinutos);
         viewHolder.tv_segundos = findViewById(R.id.tv_segundos);
+        viewHolder.tv_nivelBateria = findViewById(R.id.tv_nivelBateria);
+        viewHolder.cb_nivelBateria = findViewById(R.id.cb_nivelBateria);
+        viewHolder.iv_preferencias = findViewById(R.id.iv_preferencias);
+        viewHolder.iv_sair = findViewById(R.id.iv_sair);
+        viewHolder.ll_menu = findViewById(R.id.ll_menu);
 //parte 2 Atualizar hora
         //parte 4 metodo vai para dentro do onResume()
-       // AtualizarHorario();
+        // AtualizarHorario();
 
 //
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//parte v1.1
+        registerReceiver(bateriaReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+        viewHolder.cb_nivelBateria.setChecked(true);
+
+
+        viewHolder.cb_nivelBateria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bateriaChecked) {
+                    bateriaChecked = false;
+                    viewHolder.tv_nivelBateria.setVisibility(View.GONE);
+
+                } else {
+                    bateriaChecked = true;
+                    viewHolder.tv_nivelBateria.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        viewHolder.ll_menu.animate().translationY(500);
+
+        viewHolder.iv_sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewHolder.ll_menu.animate()
+                        .translationY(viewHolder.ll_menu.getMeasuredHeight())
+                        .setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
+
+            }
+        });
+
+        viewHolder.iv_preferencias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewHolder.ll_menu.setVisibility(View.VISIBLE);
+                viewHolder.ll_menu.animate()
+                        .translationY(0)
+                        .setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
+            }
+        });
+//
+
+
     }
 
 
-//parte 4
+    //parte 4
     @Override
     protected void onResume() {
         super.onResume();
-        runnableStopped=false;
+        runnableStopped = false;
         AtualizarHorario();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        runnableStopped=true;
+        runnableStopped = true;
     }
+
     //
 /*parte 3
     private void AtualizarHorario() {
@@ -84,7 +156,7 @@ public class FullscreenActivity2 extends AppCompatActivity {
             @Override
             public void run() {
                 //parte 5
-                if(runnableStopped)
+                if (runnableStopped)
                     return;
                 //
                 Calendar calendar = Calendar.getInstance();
@@ -97,10 +169,14 @@ public class FullscreenActivity2 extends AppCompatActivity {
                 viewHolder.tv_segundos.setText(segundosFormatado);
                 //parte 3 handler
                 long agora = SystemClock.uptimeMillis();
-                long proximo = agora + (1000-(agora%1000));
+                long proximo = agora + (1000 - (agora % 1000));
 
                 handler.postAtTime(runnable, proximo);
                 //
+                //parte v1.1
+                viewHolder.tv_nivelBateria = findViewById(R.id.tv_nivelBateria);
+                viewHolder.cb_nivelBateria = findViewById(R.id.cb_nivelBateria);
+//
             }
         };
         runnable.run();
@@ -117,8 +193,15 @@ public class FullscreenActivity2 extends AppCompatActivity {
 
     }
 
-    private static class ViewHolder{
+    private static class ViewHolder {
         TextView tv_horasMinutos;
         TextView tv_segundos;
+        //parte v1.1
+        CheckBox cb_nivelBateria;
+        TextView tv_nivelBateria;
+        ImageView iv_sair;
+        ImageView iv_preferencias;
+        LinearLayout ll_menu;
+
     }
 }
